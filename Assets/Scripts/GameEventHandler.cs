a66d0697ecbd7f1584aee54c6073aeb9d35b3429
markus.liuska <markus.liuska@gmail.com>
2022-01-29 21:57:54 +0200

    Added code and prefab for fail state handling

---
+++ Assets/Scripts/GameEventHandler.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+
+public class GameEventHandler : MonoBehaviour
+{
+    public GameObject MainCamera;
+    public GameObject FailCurtain;
+    public GameObject FailTextGO; 
+
+    private TextMeshProUGUI failTextTMPro;
+    bool failedState = false;
+
+    private void Start() {
+        FailCurtain.SetActive(false);
+        failTextTMPro = FailTextGO.GetComponent<TextMeshProUGUI>();
+        if (failTextTMPro == null){
+            Debug.LogError("Failed to find required TextMeshPro component");
+        }
+        failTextTMPro.SetText("Haha");
+    }
+
+
+    void Update (){
+
+        if(!failedState){
+            return;
+        }
+
+        if(Input.anyKey){
+            ReloadScene();
+        }
+
+    }
+
+
+    [ContextMenu("Debug OnFailEvent")]
+    private void DebugOnFailEvent(){
+        Debug.Log("DebugonFailEvent -> Simulating OnFailEvent..");
+        OnFailEvent("Testing data");
+    }
+
+
+    public void OnFailEvent(string failReasonMessage){
+
+        Debug.LogFormat("Player failed, reason message: {0}", failReasonMessage);
+        FailCurtain.SetActive(true);
+        failTextTMPro.SetText(failReasonMessage.ToUpper());
+        failedState = true;
+
+
+    }
+
+
+    public void ReloadScene()
+     {
+         Scene scene = SceneManager.GetActiveScene();
+         SceneManager.LoadScene(scene.name);
+     }
+}
