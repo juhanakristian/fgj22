b4b1b6e19663d19117c362444a23985a24da9edb
laptop <joel.polso@analogway.com>
2022-01-29 16:32:40 +0900

    Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public new Transform camera;
+	public Transform anchor;
+
+	public float yOffset = 1.0f;
+	public float zOffset = 1.0f;
+
+	public float pitch = 0.0f;
+	public float yaw = 0.0f;
+
+	public float maxPitch = 90.0f;
+	public float minPitch = -90.0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		if (!camera) {
+			camera = GetComponent<Camera>().transform;
+			pitch = camera.eulerAngles.x;
+			yaw = camera.eulerAngles.x;
+		}
+
+		if (!anchor) {
+			anchor = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+		}
+	}
+
+	void OnEnable()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void OnDisable()
+	{
+		Cursor.lockState = CursorLockMode.None;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		camera.transform.position = anchor.position + new Vector3(0, yOffset, 0) - camera.forward * zOffset;
+
+		float mx = Input.GetAxis("Mouse X");
+		float my = Input.GetAxis("Mouse Y");
+
+		pitch -= my;
+		yaw   += mx;
+
+		pitch = Mathf.Min(pitch, maxPitch);
+		pitch = Mathf.Max(pitch, minPitch);
+
+		camera.eulerAngles = new Vector3(pitch, yaw, 0);
+	}
+}
