7bbe9a44c6751f25d954a0a7260ce7d298201ac0
Joel Polso <joel.polso@analogway.com>
2022-01-29 16:16:07 +0200

    Coffee machine script updates

--- Assets/Scripts/CoffeeMachine.cs
+++ Assets/Scripts/CoffeeMachine.cs
@@ -6,17 +6,35 @@ using UnityEngine.Events;
 public class CoffeeMachine : MonoBehaviour
 {
 	public UnityEvent brewingComplete; 
+	public float brewingDuration = 5.0f;
+	public float brewingTimer = 0.0f;
+	public bool brewing = false;
+
+	private IEnumerator coro;
 
 	public void StartBrewing()
 	{
+		StopBrewing();
+
+		// Already brewed?
+		if (brewingTimer >= brewingDuration)
+			return;
+
 		Debug.Log("Brewing...", this);
-		Debug.Log("Brewing done", this);
-		brewingComplete.Invoke();
+		coro = Brew();
+		brewing = true;
+		StartCoroutine(coro);
 	}
 
 	public void StopBrewing()
 	{
+		if (!brewing)
+			return;
+
+		brewing = false;
 		Debug.Log("Brewing canceled", this);
+
+		StopCoroutine(coro);
 	}
 
 	// Start is called before the first frame update
@@ -28,5 +46,16 @@ public class CoffeeMachine : MonoBehaviour
 	void Update()
 	{
 
+	}
+
+	private IEnumerator Brew()
+	{
+		while (brewingTimer < brewingDuration) {
+			yield return new WaitForSeconds(0.1f);
+			brewingTimer += 0.1f;
+		}
+
+		Debug.Log("Brewing done", this);
+		brewingComplete.Invoke();
 	}
 }
