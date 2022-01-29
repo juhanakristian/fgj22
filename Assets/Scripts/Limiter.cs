289276fd465c359187b31e54e9aeb9d97d649791
markus.liuska <markus.liuska@gmail.com>
2022-01-29 14:25:34 +0200

    added menu scene and related dependencies

---
+++ Assets/Scripts/Limiter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limiter : MonoBehaviour
+{
+  
+    public int targetFrameRate = 30;
+ 
+     private void Start()
+     {
+         QualitySettings.vSyncCount = 0;
+         Application.targetFrameRate = targetFrameRate;
+     }
+}
